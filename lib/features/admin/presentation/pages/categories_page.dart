b0161ad88c43653216e5932d82a73526abb60bdc
kuroro <fernandovalenzuelaibarra@gmail.com>
2026-05-01 16:13:19 -0400

    fix(categories): add missing AppTheme import

--- lib/features/admin/presentation/pages/categories_page.dart
+++ lib/features/admin/presentation/pages/categories_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../core/theme/app_theme.dart';
 import '../../domain/entities/category.dart';
 import '../bloc/category_bloc.dart';
 import '../bloc/category_event.dart';
